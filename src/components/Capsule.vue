17db7cb6bd6bfa4e0e9fa4883335752729bb96a0
Juan <juan@agence-polux.fr>
2018-10-05 21:32:16 +0200

    remove last rem uses in preference to em

--- src/components/Capsule.vue
+++ src/components/Capsule.vue
@@ -25,19 +25,20 @@ export default
 </script>
 
 <style scoped lang="scss">
+  $pfbgcolor : yellow;
   @import '@/assets/sass/_0.declare.scss';
   span
   {
     background-color:$pfbgcolor;
     line-height:1;
     display:inline-block;
-    padding:0.2rem 0.5rem;
+    padding:0.2em 0.5em;
     vertical-align:middle;
-    border-radius:2rem;
+    border-radius:2em;
     font-weight:normal;
-    font-size:1rem;
+    font-size:1em;
     font-family:$crustsemibold;
     text-transform:uppercase;
-    margin-left:1rem;
+    margin-left:1em;
   }
 </style>
