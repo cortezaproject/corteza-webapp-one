d029dce3621761b0161c32ca0f6ef72e76b56c4e
Juan <juan@agence-polux.fr>
2018-10-08 21:12:19 +0200

    removing "basic input component"

--- src/components/form-elements/Todo.vue
+++
@@ -1,5 +0,0 @@
-// définir plusieurs éléments dans une librairie
-
-// https://alligator.io/vuejs/global-event-bus/  ?
-
-// calcul d'un élement en fonction d'un autre (pour la séparation verticale du chrome)
