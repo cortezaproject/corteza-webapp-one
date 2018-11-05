c8d9e4952f96aaf90b19522afc8f1f82c3e649ed
Tit Petric <black@scene-si.org>
2018-11-05 15:17:23 +0100

    add store to plugins if needed

--- src/plugins/index.js
+++ src/plugins/index.js
@@ -1,9 +1,10 @@
+import store from '@/store'
 import Vue from 'vue'
 
 import system from '@/plugins/system'
-import crm from '@/plugins/crm'
 import sam from '@/plugins/sam'
+import crm from '@/plugins/crm'
 
-Vue.use(system)
-Vue.use(crm)
-Vue.use(sam)
+Vue.use(system, store)
+Vue.use(crm, store)
+Vue.use(sam, store)
