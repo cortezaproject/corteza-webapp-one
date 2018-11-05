730897729349cea24d2b2ea78267763f9729faeb
Tit Petric <black@scene-si.org>
2018-11-05 15:23:29 +0100

    reset custom lint configs added by juan

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,9 @@
 module.exports = {
-  root: true,
+  root: false,
   env: {
     node: true,
   },
-  'extends': [
+  extends: [
     'plugin:vue/essential',
     '@vue/standard',
   ],
@@ -11,8 +11,6 @@ module.exports = {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'comma-dangle': ['error', 'always-multiline'],
-    'brace-style': 'off',
-    'yoda': 'off',
   },
   parserOptions: {
     parser: 'babel-eslint',
