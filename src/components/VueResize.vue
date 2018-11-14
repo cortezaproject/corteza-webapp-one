133540cf20d2b6a6366f278fef5c3987c7bf4323
tmcdos <tmcdos@abv.bg>
2018-11-14 14:35:24 +0200

    The dashed outline for VueDragResize is drawn by Firefox so that it includes the draggable circular stick - replaced with a border

--- src/components/VueResize.vue
+++ src/components/VueResize.vue
@@ -103,11 +103,6 @@ export default
     }
   }
 
-  .vdr.vdr-panel.active
-  {
-    outline: 1px dashed #D6D6D6;
-  }
-
   .resizable-panel-content
   {
     min-height: 0;
@@ -115,6 +110,7 @@ export default
     overflow: hidden;
     display: flex;
     flex-direction: column;
+    border: 1px dashed #D6D6D6;
   }
 
   .resizable-panel-content > *:nth-child(2)
