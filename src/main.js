15392888e03f542ae02e675cf190f5760aa99e26
Tit Petric <black@scene-si.org>
2018-11-16 15:42:18 +0100

    add logger, update scss to match crm/other apps

--- src/main.js
+++ src/main.js
@@ -3,14 +3,24 @@ import Index from './views/Index.vue'
 
 import './global'
 import './plugins'
-import './assets/main.scss'
+import './main.scss'
 
 import router from './router'
 import store from './store'
+import logger from './logger'
 
-// implement a Vue with store and router
-new Vue({
-  router,
-  store,
-  render: h => h(Index),
-}).$mount('#app')
+/* eslint-disable no-undef */
+logger.log(
+  `%cCrust CRM, version: ${CRUST_VERSION}, build time: ${CRUST_BUILD_TIME}`,
+  'background-color: #1397CB; color: white; padding: 3px 10px; border: 1px solid black; font: Courier',
+)
+
+if (window.CrustConfig === undefined) {
+  alert('Unexisting or invalid configuration. Make sure there is a public/config.js configuration file.')
+} else {
+  new Vue({
+    router,
+    store,
+    render: h => h(Index),
+  }).$mount('#app')
+}
