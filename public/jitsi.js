bec4afceb67637d5624c2d00439be3c3dbb64108
Richard <i-am-dev@users.noreply.github.com>
2019-01-03 23:16:33 +0200

    Adjusted Jitsi interface config options

--- public/jitsi.js
+++ public/jitsi.js
@@ -6,20 +6,28 @@ var options = {
   height: '100%',
   parentNode: document.querySelector('#meet'),
   interfaceConfigOverwrite: {
+    DEFAULT_BACKGROUND: '#232323',
     SHOW_JITSI_WATERMARK: false,
     SHOW_WATERMARK_FOR_GUESTS: false,
+    SHOW_BRAND_WATERMARK: false,
+    BRAND_WATERMARK_LINK: '',
+    SHOW_POWERED_BY: false,
+    DEFAULT_REMOTE_DISPLAY_NAME: 'Fellow Cruster',
+    DEFAULT_LOCAL_DISPLAY_NAME: 'me',
     TOOLBAR_BUTTONS: [
       'microphone', 'camera', 'closedcaptions', 'desktop', 'fullscreen',
       'fodeviceselection', 'hangup', 'profile', 'info', 'recording',
-      'settings',
-      'videoquality', 'filmstrip', 'invite', 'shortcuts',
+      'settings', 'tileview', 'videoquality', 'filmstrip', 'invite',
+      'shortcuts',
     ],
+    SETTINGS_SECTIONS: ['devices', 'language', 'moderator', 'profile', 'calendar'],
   },
 }
 var api = new JitsiMeetExternalAPI(domain, options)
 
 document.addEventListener('unload', exitRoom)
 
-function exitRoom () {
+function exitRoom() {
   api.dispose()
 }
+
