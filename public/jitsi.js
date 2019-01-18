74449f1890c842a8aaece0a13f987be352e8de80
Richard <i-am-dev@users.noreply.github.com>
2019-01-18 13:28:20 +0200

    Jitsi room selection now has access to list of public messaging channels

--- public/jitsi.js
+++ public/jitsi.js
@@ -1,24 +1,38 @@
-var domain = 'meet.jit.si'
+var domain = 'meet.jit.si' // 'localhost:8443' // 
 var urlParams = new URLSearchParams(window.location.search)
 var options = {}
-var api, room, user
+var api, room, user, crustMessageChannels, channels
 
 document.addEventListener('DOMContentLoaded', function () {
-
   room = urlParams.get('id') || 'CrustJitsi'
   user = urlParams.get('user') || 'me'
 
-  ListJitsiRooms()
+  // see if storage has list of crust rooms
+  crustMessageChannels = JSON.parse(localStorage.getItem('crustMessagingChannels'))
+  // console.log(crustMessageChannels)
+  channels = []
+  crustMessageChannels.forEach(function (channel) {
+    if (channel.type === 'public' && channel.canJoin === true) {
+      var ch = {
+        id: channel.ID,
+        name: channel.name.substring(0, 45),
+        value: channel.name.substring(0, 30).replace(/\(.+?\)/g, '').replace(/[^a-z0-9+]+/gi, ''),
+        memberCount: channel.members.length,
+      }
+      channels.push(ch)
+    }
+  })
 
-});
+  ListJitsiRooms()
+})
 
 document.addEventListener('unload', exitRoom)
 
-function exitRoom() {
+function exitRoom () {
   api.dispose()
 }
 
-function LoadJitsiRoom(room, user) {
+function LoadJitsiRoom (room, user) {
   options = {
     roomName: room,
     width: '100%',
@@ -45,33 +59,43 @@ function LoadJitsiRoom(room, user) {
   api = new JitsiMeetExternalAPI(domain, options)
 }
 
-function SelectRoomClick(room) {
-  document.getElementById("roomselection").style.display = "none"
+function SelectRoomClick (room) {
+  document.getElementById('roomselection').style.display = 'none'
+  LoadJitsiRoom(room, user)
+}
+
+function SelectRoomDropdown (item) {
+  document.getElementById('roomselection').style.display = 'none'
+  LoadJitsiRoom(item.value, user)
+}
+
+function SubmitRoomCreator () {
+  room = 'crust' + document.getElementById('roomInputField').value.replace(/\(.+?\)/g, '').replace(/[^a-z0-9+]+/gi, '')
+  document.getElementById('roomselection').style.display = 'none'
   LoadJitsiRoom(room, user)
 }
 
 function ListJitsiRooms() {
-  var roomlist = ['crustroom1', 'crustroom2', 'crustroom3']
-  var list = "<div id='roomlist'><ul>"
-  list += "<li onClick='SelectRoomClick(\"" + roomlist[0] + "\")'>Room 1</li>"
-  list += "<li onClick='SelectRoomClick(\"" + roomlist[1] + "\")'>Room 2</li>"
-  list += "<li onClick='SelectRoomClick(\"" + roomlist[2] + "\")'>Room 3</li>"
-  list += "</ul></div>"
+  var list = '<div id="roomdropdown"><select id="roomdropdownselect" onchange="SelectRoomDropdown(this)">'
+  list += '<option value="">Select a room</option>'
+  channels.forEach(function (channel) {
+    list += '<option value="crust' + channel.value + '">' + channel.name + '</option>'
+  })
+  list += '</select></div>'
 
-  ReplaceElementContentByID('roomlist', list)
+  ReplaceElementContentByID('roomdropdown', list)
 }
 
-function ReplaceElementContentByID(id, content) {
-  var Obj = document.getElementById(id); //any element to be fully replaced
+function ReplaceElementContentByID (id, content) {
+  var Obj = document.getElementById(id) // any element to be fully replaced
 
-  if (Obj.outerHTML) { //if outerHTML is supported
-    Obj.outerHTML = content; ///it's simple replacement of whole element with contents of str var
-  }
-  else { //if outerHTML is not supported, there is a weird but crossbrowsered trick
-    var tmpObj = document.createElement("div");
-    tmpObj.innerHTML = '<!--THIS DATA SHOULD BE REPLACED-->';
-    ObjParent = Obj.parentNode; //Okey, element should be parented
-    ObjParent.replaceChild(tmpObj, Obj); //here we placing our temporary data instead of our target, so we can find it then and replace it into whatever we want to replace to
-    ObjParent.innerHTML = ObjParent.innerHTML.replace('<div><!--THIS DATA SHOULD BE REPLACED--></div>', content);
+  if (Obj.outerHTML) { // if outerHTML is supported
+    Obj.outerHTML = content /// it's simple replacement of whole element with contents of str var
+  } else { // if outerHTML is not supported, there is a weird but crossbrowsered trick
+    var tmpObj = document.createElement('div')
+    tmpObj.innerHTML = '<!--THIS DATA SHOULD BE REPLACED-->'
+    var ObjParent = Obj.parentNode // Okey, element should be parented
+    ObjParent.replaceChild(tmpObj, Obj) // here we placing our temporary data instead of our target, so we can find it then and replace it into whatever we want to replace to
+    ObjParent.innerHTML = ObjParent.innerHTML.replace('<div><!--THIS DATA SHOULD BE REPLACED--></div>', content)
   }
 }
