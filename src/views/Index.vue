2b31867ace1479a61a133f16c467c258f299e57e
Mia Arh <mia@kendu.si>
2019-06-19 18:42:19 +0200

    Rename Crust to Corteza

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,31 +1,16 @@
 <template>
-  <div id="unify" class="crust">
+  <div id="unify">
     <router-multi-view/>
   </div>
 </template>
-
-<script>
-export default {
-
-}
-</script>
 <style lang="scss">
-  html *
-  {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-  }
-
-  body
-  {
-    padding: 0;
-    margin: 0;
-  }
-
-  #unify > *
-  {
-    width: 100vw;
-    height: 100vh;
-  }
+html * {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+#unify > * {
+  width: 100vw;
+  height: 100vh;
+}
 </style>
