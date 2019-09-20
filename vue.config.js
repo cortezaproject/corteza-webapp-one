067d3be8cc184adc41e774d9daf0b15874bd4112
Jože Fortun <joze.fortun@gmail.com>
2019-09-20 11:18:57 +0200

    Add appFlavour to build variables

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 const buildVueConfig = require('./vue.config-builder')
 
 module.exports = buildVueConfig({
+  appFlavour: 'Corteza',
   appName: 'one',
   appLabel: 'Corteza One',
   theme: 'corteza-base',
