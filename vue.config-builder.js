1c0c75f0b13a467546f6b4d3bce7be0cb3893154
Marko Sabec <marko@sabec.si>
2021-05-09 22:32:21 +0200

    Add Github actions CI pipeline

--- vue.config-builder.js
+++ vue.config-builder.js
@@ -1,5 +1,4 @@
 var webpack = require('webpack')
-var exec = require('child_process').execSync
 var path = require('path')
 
 module.exports = ({ appFlavour, appName, appLabel, version, theme, packageAlias, root = path.resolve('.'), env = process.env.NODE_ENV }) => {
@@ -37,7 +36,7 @@ module.exports = ({ appFlavour, appName, appLabel, version, theme, packageAlias,
         new webpack.DefinePlugin({
           FLAVOUR: JSON.stringify(appFlavour),
           WEBAPP: JSON.stringify(appLabel),
-          VERSION: JSON.stringify(version || ('' + exec('git describe --always --tags')).trim()),
+          VERSION: JSON.stringify(version),
           BUILD_TIME: JSON.stringify((new Date()).toISOString()),
         }),
       ],
