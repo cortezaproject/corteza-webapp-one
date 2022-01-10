47a561f8a3a283c937acaa83455c4317f718f3cc
Jože Fortun <joze.fortun@gmail.com>
2022-01-10 18:13:12 +0100

    Fix RTL support

--- src/mixins/index.js
+++ src/mixins/index.js
@@ -1,1 +1,5 @@
-// import Vue from 'vue'
+import Vue from 'vue'
+
+import resourceTranslations from './resource-translations'
+
+Vue.mixin(resourceTranslations)
--- src/mixins/resource-translations.js
+++ src/mixins/resource-translations.js
@@ -1,36 +1,5 @@
 export default {
   computed: {
-    /**
-     * Keep this inside methods otherwise it (no-value) gets caught by Vue
-     * since settings are actually fetched a tad later...
-     *
-     * @returns {string[]}
-     */
-    resourceTranslationLanguages () {
-      const ll = this.$Settings.get('resourceTranslations.languages')
-      if (!ll || !Array.isArray(ll) || ll.length === 0) {
-        return ['en']
-      }
-
-      return ll
-    },
-
-    /**
-     * Resource translations are enabled when there is more than 1 language
-     * @returns {boolean}
-     */
-    resourceTranslationsEnabled () {
-      return this.resourceTranslationLanguages.length > 1
-    },
-
-    /**
-     * 1st language is always default
-     * @returns {*}
-     */
-    defaultTranslationLanguage () {
-      return this.resourceTranslationLanguages[0]
-    },
-
     currentLanguage () {
       return this.$i18n.i18next.language
     },
@@ -55,7 +24,7 @@ export default {
         case 'sd':
           return 'rtl'
         default:
-          return 'rtl'
+          return 'ltr'
       }
     },
   },
