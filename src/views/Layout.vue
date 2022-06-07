6e835bc3e3aea10208323ea3a9e5cc55c7a0e295
Jože Fortun <joze.fortun@gmail.com>
2022-06-07 12:42:27 +0200

    Add prompts

--- src/views/Layout.vue
+++ src/views/Layout.vue
@@ -1,43 +1,50 @@
 <template>
   <div
-    v-if="loaded"
     class="d-flex flex-column w-100 vh-100"
   >
-    <header
-      class="mw-100"
+    <div
+      v-show="loaded"
     >
-      <c-topbar
-        hide-app-selector
-        :sidebar-pinned="pinned"
-        :settings="$Settings.get('ui.topbar', {})"
-        :labels="{
-          helpForum: $t('help.forum'),
-          helpDocumentation: $t('help.documentation'),
-          helpFeedback: $t('help.feedback'),
-          helpVersion: $t('help.version'),
-          userSettingsLoggedInAs: $t('userSettings.loggedInAs', { user }),
-          userSettingsProfile: $t('userSettings.profile'),
-          userSettingsChangePassword: $t('userSettings.changePassword'),
-          userSettingsLogout: $t('userSettings.logout'),
-        }"
+      <header
+        class="mw-100"
       >
-        <template #help-dropdown>
-          <portal-target name="topbar-help-dropdown" />
-        </template>
-      </c-topbar>
-    </header>
+        <c-topbar
+          hide-app-selector
+          :sidebar-pinned="pinned"
+          :settings="$Settings.get('ui.topbar', {})"
+          :labels="{
+            helpForum: $t('help.forum'),
+            helpDocumentation: $t('help.documentation'),
+            helpFeedback: $t('help.feedback'),
+            helpVersion: $t('help.version'),
+            userSettingsLoggedInAs: $t('userSettings.loggedInAs', { user }),
+            userSettingsProfile: $t('userSettings.profile'),
+            userSettingsChangePassword: $t('userSettings.changePassword'),
+            userSettingsLogout: $t('userSettings.logout'),
+          }"
+        >
+          <template #help-dropdown>
+            <portal-target name="topbar-help-dropdown" />
+          </template>
+        </c-topbar>
+      </header>
 
-    <main class="d-flex h-100 overflow-hidden">
-      <c-app-selector
-        :logo="logo"
-      />
-    </main>
+      <main class="d-flex h-100 overflow-hidden">
+        <c-app-selector
+          :logo="logo"
+        />
+      </main>
+    </div>
+
+    <c-loader-logo
+      v-if="!loaded"
+      :logo="logo"
+    />
+
+    <c-prompts
+      :hide-toasts="!loaded"
+    />
   </div>
-
-  <c-loader-logo
-    v-else
-    :logo="logo"
-  />
 </template>
 
 <script>
@@ -45,7 +52,7 @@ import { mapActions } from 'vuex'
 import CAppSelector from '../components/CAppSelector'
 import { components } from '@cortezaproject/corteza-vue'
 
-const { CTopbar, CLoaderLogo } = components
+const { CTopbar, CLoaderLogo, CPrompts } = components
 
 export default {
   i18nOptions: {
@@ -56,6 +63,7 @@ export default {
     CAppSelector,
     CTopbar,
     CLoaderLogo,
+    CPrompts,
   },
 
   data () {
@@ -94,11 +102,12 @@ export default {
   },
 
   created () {
-    this.loaded = false
-
-    this.preloadApplications().then(() => {
-      this.loaded = true
-    })
+    this.preloadApplications()
+      .finally(() => {
+        setTimeout(() => {
+          this.loaded = true
+        }, 500)
+      })
   },
 
   methods: {
